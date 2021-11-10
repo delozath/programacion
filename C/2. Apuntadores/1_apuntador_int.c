f1051ae4953c1fcf08a440c7c034d8e898e92293
delozath@gmail.com <delozath@gmail.com>
2021-11-09 20:01:14 -0600

    update formato 1_apuntador.c

--- C/2. Apuntadores/1_apuntador_int.c
+++ C/2. Apuntadores/1_apuntador_int.c
@@ -6,22 +6,22 @@ void imprime_int(int *px);
 void main(){
   int x;
   
-  printf("\n\n Bloque 1");
+  printf("\n\n Bloque 1\n");
   captura_int(&x);
   printf("\n  x = %d\n", x);
   getchar();
   
-  printf("\n\n Bloque 2");
+  printf("\n\n Bloque 2\n");
   int *pointer_x = &x;
   printf("\n  pointer = %ld\n", (long int)pointer_x);
   getchar();
   
-  printf("\n\n Bloque 3");
+  printf("\n\n Bloque 3\n");
   captura_int(pointer_x);
   printf("\n  x = %d\n", x);
   getchar();
   
-  printf("\n\n Bloque 4");
+  printf("\n\n Bloque 4\n");
   imprime_int(pointer_x);
 }
 
